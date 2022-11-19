2aee2822f684703c0f54f4fa7c4388e1271d2632
zwardtooommy <870553699@qq.com>
2022-11-19 22:33:23 +0800

    change 1 twice

--- 1.c
+++ 1.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 void update(int &a);
-void update(int &a)
-{
-    return;
-}
 int main()
 {
     int a[]={1,2,3,4,5};
     int *p=a;
     cout<<*p;
     return 0;
+}
+
+void update(int &a)
+{
+    return;
 }
